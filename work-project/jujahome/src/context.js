badf8e660473a3aa7f2a44452e5849849f74c451
Sil silas <silaswans@gmail.com>
2020-08-07 02:03:49 +0300

    single room

--- work-project/jujahome/src/context.js
+++ work-project/jujahome/src/context.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react'
+import items from './data'
 
 
 const RoomContext = React.createContext();
 
  class RoomProvider extends Component {
-    state={}
+    state={
+       rooms:[],
+       sortedRooms:[],
+       featuredRooms:[],
+       loading: true
+    }
+
+
+    componentDidMount(){
+        let rooms = this.formartData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
+
+    }
+
+
+    formartData(items){
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = {...item.fields, images, id}
+
+            return room;
+        })
+        return tempItems;
+    }
+
+
+    getRooms = (slug) => {
+       let tempRooms = [...this.state.rooms];
+       const room = tempRooms.find((room) => room.slug === slug)
+       return room;
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={"halloh"} >
+            <RoomContext.Provider
+             value={{...this.state,
+                 getRooms:this.getRooms
+                }}
+             >
                 {this.props.children}
             </RoomContext.Provider>
         )
--- work-project/jujahome/src/pages/SingleRoom.js
+++ work-project/jujahome/src/pages/SingleRoom.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
+import  Burner from '../components/Burner'
+import {RoomContext} from '../context'
 
 export default class SingleRoom extends Component {
     render() {
         return (
             <div>
-               sigle rooom
+
             </div>
         )
     }
