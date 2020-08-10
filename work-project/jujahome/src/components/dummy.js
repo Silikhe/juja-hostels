0fdc15f8379ed98868a91752b183f9ffc8baec6b
Sil silas <silaswans@gmail.com>
2020-08-10 23:40:51 +0300

    intial comit

---
+++ work-project/jujahome/src/components/dummy.js
@@ -0,0 +1,36 @@
+cg from '../images/room-1.jpeg'
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
+import  Burner from '../components/Burner'
+import {RoomContext} from '../context'
+
+export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+    //   console.log(this.props)
+    this.state={
+        slug:this.props.match.param.slug,
+        defaultBcg
+     }
+    }
+
+    static contextType = RoomContext
+
+
+    // componentDidMount(){}
+    render() {
+        const {getRoom}  = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room){
+            return
+            <div className="error">
+                <h1>No such room could be found</h1>
+            </div>
+        }
+        return (
+            <div>
+            Hello
+            </div>
+        )
+    }
+}
