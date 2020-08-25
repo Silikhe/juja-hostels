118049c0f7718c9370c42637feb3853d4f35d3f7
Sil silas <silaswans@gmail.com>
2020-08-25 23:57:36 +0300

    card view

--- work-project/jujahome/src/Contentful.js
+++ work-project/jujahome/src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space:"h8o9e1v3kjpy",
-    accessToken:"OK3P-0IUeT90ZYkZW6hiRjqrAlhrWSD30ZqtcUZKU1s"
+    space:process.env.REACT_APP_API_SPACE,
+    accessToken:process.env.REACT_APP_ACCESS_TOKEN
 })
